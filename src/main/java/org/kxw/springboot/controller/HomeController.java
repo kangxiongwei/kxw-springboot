76eb947f18f47c5954843edfd917adfbd2b830f2
亢雄伟 <kangxiongwei@meituan.com>
2018-06-29 13:41:07 +0800

    SpringBoot的初始化Web应用，欢迎页和分环境

---
+++ src/main/java/org/kxw/springboot/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.kxw.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Create by kangxiongwei on 2018/6/28 19:33.
+ */
+@Controller
+public class HomeController {
+
+
+
+}
